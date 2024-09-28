3e662df40e6c03bd7797f72f5d62027a62a2a3a1
tsenchung <tsen.fchung@gmail.com>
2024-09-27 18:42:13 -0600

    Implements a basic metronome (#1)

---
+++ src/additional-svelte-typings.d.ts
@@ -0,0 +1,15 @@
+declare namespace svelteHTML {
+	interface SVGAttributes<T extends EventTarget> extends AriaAttributes, DOMAttributes<T> {
+		"xmlns:cc"?: string;
+        "xmlns:dc"?: string;
+        "xmlns:rdf"?: string;
+        "xmlns:svg"?: string;
+        "xmlns:sodipodi"?: string;
+        "xmlns:inkscape"?: string;
+        "sodipodi:docname"?: string;
+        "inkscape:version"?: string;
+        "inkscape:label"?: string;
+        "inkscape:groupmode"?: string;
+        "inkscape:connector-curvature"?: string;
+	}
+}
---
+++ src/lib/icons/SixteenthNote.svelte
@@ -0,0 +1,78 @@
+<!-- Actormusicus, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons / Adapted as a tailwind icon in svelte -->
+<!-- svelte-ignore illegal-attribute-character -->
+
+<svg
+	xmlns:dc="http://purl.org/dc/elements/1.1/"
+	xmlns:cc="http://creativecommons.org/ns#"
+	xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+	xmlns:svg="http://www.w3.org/2000/svg"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+	xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+	class="size-8"
+	viewBox="0 0 15.697994 26.906273"
+	version="1.1"
+	id="svg4416"
+	sodipodi:docname="Semicroma.svg"
+	inkscape:version="0.92.3 (2405546, 2018-03-11)"
+>
+	<defs id="defs4410" />
+	<sodipodi:namedview
+		id="base"
+		pagecolor="#ffffff"
+		bordercolor="#666666"
+		borderopacity="1.0"
+		inkscape:pageopacity="0.0"
+		inkscape:pageshadow="2"
+		inkscape:zoom="0.35"
+		inkscape:cx="-619.83849"
+		inkscape:cy="-65.551324"
+		inkscape:document-units="mm"
+		inkscape:current-layer="layer1"
+		showgrid="false"
+		units="px"
+		inkscape:window-width="1366"
+		inkscape:window-height="700"
+		inkscape:window-x="0"
+		inkscape:window-y="0"
+		inkscape:window-maximized="1"
+	/>
+	<metadata id="metadata4413">
+		<rdf:RDF>
+			<cc:Work rdf:about="">
+				<dc:format>image/svg+xml</dc:format>
+				<dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+				<dc:title />
+			</cc:Work>
+		</rdf:RDF>
+	</metadata>
+	<g
+		inkscape:label="Livello 1"
+		inkscape:groupmode="layer"
+		id="layer1"
+		transform="translate(0,-270.0937)"
+	>
+		<g id="g5156" transform="matrix(0.26458333,0,0,0.26458333,-274.95216,174.94051)">
+			<polyline
+				id="polyline62"
+				points="1071.08,444.895 1071.08,361.417"
+				class="Stem"
+				style="fill:none;stroke:#000000;stroke-width:2.73000002;stroke-linejoin:bevel"
+			/>
+			<path
+				id="path88"
+				d="M 31.5,-8.20313 C 30,-10.9063 27.1094,-12.5938 23.2031,-13 c -0.6093,-0.0938 -1.1093,-0.0938 -1.7031,-0.0938 -3.5,0 -7.2969,1 -10.7969,2.8907 C 4.09375,-6.59375 0,-0.703125 0,4.40625 c 0,1.39063 0.203125,2.6875 0.90625,3.89063 1.5,2.70312 4.5,4.40622 8.39063,4.79692 0.60937,0.1093 1.10942,0.1093 1.70312,0.1093 3.5,0 7.2031,-1 10.7031,-2.9062 C 28.2969,6.70313 32.5,0.90625 32.5,-4.20313 c 0,-1.39062 -0.2969,-2.79687 -1,-4"
+				transform="matrix(0.992126,0,0,0.992126,1040.21,448.228)"
+				class="Note"
+				inkscape:connector-curvature="0"
+			/>
+			<path
+				id="path162"
+				d="M 28,45.5 C 27.9063,40.2969 26.7031,34.9063 23.2031,30.4063 18.4063,24.2031 12.0938,21.0938 8.29688,15.5938 4.5,10.0938 2.90625,-0.59375 2.90625,-0.59375 2.79688,-1.29688 2.59375,-1.79688 1.40625,-1.79688 0.203125,-1.79688 0,-1.29688 0,-0.59375 v 15.89065 5.9062 11.7032 4.7968 c 0,0.2032 0.703125,0.7969 1.40625,1 C 4,39.7031 11.9063,44.6094 17.9063,53.5 21.5,59 22.0938,62.7031 22.0938,66.2969 v 1.1094 C 22,69.9063 21.4063,75 19.9063,79 c -0.3125,0.9063 -0.8125,2.2031 0.1875,2.7031 1,0.5 1.6093,-0.5937 2.1093,-1.2968 C 24,77.5 26.6094,72.2031 26.6094,66.5 c 0,-2.7969 -0.2031,-5.2969 -0.5,-7.3906 C 27.1094,55.5 28,51 28,46.2969 V 45.5 m -7.0937,1.4063 C 18.2031,43.4063 15.2031,40.2031 10.2969,35.2969 6.40625,31.4063 4.59375,28 3.90625,25.4063 3.79688,25.2031 3.79688,24.7969 3.70313,24.5 l 0.09375,0.4063 c 4.5,0.3906 10.60942,4.8906 14.40622,9.5 4.5,5.3906 5.3907,9.5937 5.3907,13.5 0,1 0,1.8906 -0.0938,2.8906 -0.5937,-1.0938 -1.7969,-2.8906 -2.5937,-3.8906"
+				transform="matrix(0.992126,0,0,0.992126,1069.72,361.417)"
+				class="Hook"
+				inkscape:connector-curvature="0"
+			/>
+		</g>
+	</g>
+</svg>
