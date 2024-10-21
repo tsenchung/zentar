5d677e069bfd4cb47a85de315a7f3bb54130200a
Tsen Chung <tsen.fchung@gmail.com>
2024-10-21 05:26:47 +0000

    More color changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ export default {
 					...themes['light'],
 					primary: 'oklch(44.42% 0.112 245.16)',
 					'primary-content': '#eee',
-					secondary: 'oklch(70.18% 0.1482 0)',
+					secondary: 'oklch(70.18% 0.1965 0)',
 					error: 'oklch(51.32% 0.269 22.18)'
 				}
 			}
